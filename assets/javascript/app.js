f8f23efb4dcb56843c97bd5b78e7204251656e13
Laura Fiig <Laura Fiig>
2016-11-13 22:10:01 -0600

    gif swap working.  need to fix issue with new buttons breaking gif propogation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,6 +35,11 @@ $(document).ready(function() {
     document.getElementById("newEntry").reset(); 
     //stop re-run of function at outer level (propogation!!)
     return false;
+  
+
+
+
+
   });
 
 //****************************//
@@ -43,11 +48,7 @@ $(document).ready(function() {
   $(".showGif").on("click", function() {
     //pull search term from button text
     searchName = $(this).text();
-    //log name
-    console.log(searchName)
-    //url to query Giphy API - put search term
-    //in search string, other parameters are:
-    //limit 10 pictures, public key included
+    //url to query Giphy API - put search term in search string, other parameters are: limit 10 pictures, public key included
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchName + "&limit=10&api_key=dc6zaTOxFJmzC"
     //run AJAX call
     $.ajax({url: queryURL, method: 'GET'})
@@ -56,35 +57,39 @@ $(document).ready(function() {
         //log response
         console.log(response);
         //clears previous gifs
-        $(".gifPic").remove()
+        $("img").remove()        
         //loop through object array to load gif in html
         for (i=0; i<response.data.length; i++){
-          $("#gifsHere").append('<img src=' + response.data[i].images.fixed_height_still.url + ' class="gifPic">')
-        }
-    }); 
+          $("#gifsHere").append('<img src=' + response.data[i].images.fixed_height_still.url + ' class="still" id="' + i + '">') }
+        //animate gif on click
+        $(document).on("click", ".still", function() {
+          swapVal = $("img").attr("class");
+            loc = $(this).attr("id");
+            $(this).attr("src", response.data[loc].images.fixed_height.url);
+            $(this).attr("class", "move");
+        });
+        //still gif on click
+        $(document).on("click", ".move", function() {
+          swapVal = $("img").attr("class");  
+            loc = $(this).attr("id");
+            $(this).attr("src", response.data[loc].images.fixed_height_still.url);
+            $(this).attr("class", "still");
+        });
 
+    }); 
   });
 
 //****************************//
-  
+
 });
 
-//still to be done
-//  add swap between still/annimated
-//  1.  add id on creation of [i]
-//  2.  on click replace with animated
-//  how to switch back and forth? loop?
-//  or 2 commands with different classes
-//  for animated or still
-//  OR
-//  append both with separate classes
-//  hide animated and toggle on click
-//  $("img").toggle();
+
 //issue to fix
 //  button add breaks button on click
 //  probably because of return false
 //  can remedy by having two functions
 //  or repeating the code, which is 
 //  basically the same thing, but D.R.Y.
+//  event.stopPropagation(); 
 //can I define two functions at once?
 //or define one as the other???
